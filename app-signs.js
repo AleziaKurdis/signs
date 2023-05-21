7cc28994bc149495219bcf2c7722fb97ceb7667d
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2023-05-21 13:51:44 -0400

    Add files via upload

---
+++ app-signs.js
@@ -0,0 +1,316 @@
+//
+//  app-signs.js
+//
+//  Created by Alezia Kurdis, May 11th, 2023.
+//  Copyright 2023, Overte e.V.
+//
+//  Create signs (for signalitic or for protest) in Overte.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+(function() {
+    var jsMainFileName = "app-signs.js";
+    var ROOT = Script.resolvePath('').split(jsMainFileName)[0];
+    
+    var APP_NAME = "SIGNS";
+    var APP_URL = ROOT + "signs.html";
+    var APP_ICON_INACTIVE = ROOT + "icon_inactive.png";
+    var APP_ICON_ACTIVE = ROOT + "icon_active.png";
+    var appStatus = false;
+    var channel = "org.overte.app.ak.signs";
+    var timestamp = 0;
+    var INTERCALL_DELAY = 200; //0.3 sec
+
+    var DEFAULT_DATA = {
+        "url": "",
+        "urlHistory": []
+    };
+    
+    protestSignID = Uuid.NULL;
+    
+    var tablet = Tablet.getTablet("com.highfidelity.interface.tablet.system");
+
+    tablet.screenChanged.connect(onScreenChanged);
+
+    var button = tablet.addButton({
+        text: APP_NAME,
+        icon: APP_ICON_INACTIVE,
+        activeIcon: APP_ICON_ACTIVE
+    });
+
+
+    function clicked(){
+        if (appStatus === true) {
+            tablet.webEventReceived.disconnect(onAppWebEventReceived);
+            tablet.gotoHomeScreen();
+            appStatus = false;
+        }else{
+            //Launching the Application UI.
+            tablet.gotoWebScreen(APP_URL);
+            tablet.webEventReceived.connect(onAppWebEventReceived);
+            appStatus = true;
+        }
+        
+        if (protestSignID === Uuid.NULL) {
+            button.editProperties({
+                isActive: appStatus
+            });
+        } else {
+            button.editProperties({
+                isActive: true
+            });
+        }
+
+    }
+
+    button.clicked.connect(clicked);
+
+    //This recieved the message from the UI(html) for a specific actions
+    function onAppWebEventReceived(message) {
+        if (typeof message === "string") {
+            var d = new Date();
+            var n = d.getTime();
+            var instruction = JSON.parse(message);
+            if (instruction.channel === channel) {
+                if (instruction.action === "GENERATE_PROTEST" && (n - timestamp) > INTERCALL_DELAY) {
+                    d = new Date();
+                    timestamp = d.getTime();
+                    generateProtestSign(instruction.url, instruction.shape);
+                } else if (instruction.action === "GENERATE_SIGNAGE" && (n - timestamp) > INTERCALL_DELAY) {
+                    d = new Date();
+                    timestamp = d.getTime();
+                    generateSignageSign(instruction.url, instruction.shape);
+                } else if (instruction.action === "GENERATE_FRAMED" && (n - timestamp) > INTERCALL_DELAY) {
+                    d = new Date();
+                    timestamp = d.getTime();
+                    generateFramedSign(instruction.url, instruction.shape, instruction.isEmissive);
+                } else if (instruction.action === "CLEAR_PROTEST" && (n - timestamp) > INTERCALL_DELAY) {
+                    d = new Date();
+                    timestamp = d.getTime();
+                    clearProtest();
+                } else if (instruction.action === "LOADED_GIVE_ME_THE_DATA") {
+                    sendCurrentKnownDataToUI();
+                    var state = false;
+                    if (protestSignID !== Uuid.NULL) {state = true;}
+                    var message = {
+                        "channel": channel,
+                        "action": "AVATAR_ENTITY_STATE",
+                        "state": state
+                    };
+                    tablet.emitScriptEvent(JSON.stringify(message));
+                } else if (instruction.action === "UPDATE_DATA") {
+                    Settings.setValue( channel + ".lastUsage", instruction.data );
+                } else if (instruction.action === "SELF_UNINSTALL" && (n - timestamp) > INTERCALL_DELAY) {
+                    d = new Date();
+                    timestamp = d.getTime();
+                    ScriptDiscoveryService.stopScript(Script.resolvePath(''), false);
+                }
+            }
+        }
+    }
+
+    //============ Add your application functions here ==================
+    function generateProtestSign(url, shape) {
+        var entityIDs = Entities.findEntitiesByName("-==%%! Protest Sign !%%==-", MyAvatar.position, 200, false);
+        entityIDs.forEach(function (currentEntityID) {
+            var currentEntityOwner = Entities.getEntityProperties(currentEntityID, ['owningAvatarID']).owningAvatarID;
+            if (currentEntityOwner === MyAvatar.sessionUUID && currentEntityID !== protestSignID) {
+                Entities.deleteEntity(currentEntityID);
+            }
+        });
+
+        var textures = {
+            "base_color_texture": url,
+            "normalmap_texture": ROOT + "models/woodNormal256.jpg"
+        };
+
+        var dimensions = {"x": 1, "y": 1, "z": 1};
+        switch(shape) {
+            case "S":
+                dimensions = {"x": 0.5, "y": 1.0280, "z": 0.0273};
+                break;
+            case "H":
+                dimensions = {"x": 0.5985, "y": 0.9453, "z": 0.0273};
+                break;
+            case "V":
+                dimensions = {"x": 0.3997, "y": 1.2364, "z": 0.0273};
+                break;
+        }
+        var signRotation = Quat.multiply(MyAvatar.orientation, Quat.fromVec3Degrees( { x: 90, y: 180, z: 0 } ));
+        
+        if (protestSignID !== Uuid.NULL) {
+            Entities.deleteEntity(protestSignID);
+            protestSignID = Uuid.NULL;
+        }
+        
+        protestSignID = Entities.addEntity({
+           "name": "-==%%! Protest Sign !%%==-",
+           "type": "Model",
+           "position": Vec3.sum(MyAvatar.position, Vec3.multiplyQbyV(MyAvatar.orientation, { x: 0, y: 0, z: -1 })),
+           "rotation": signRotation,
+           "dimensions": dimensions,
+           "modelURL": ROOT + "models/protest_sign_" + shape.toLowerCase() + ".fst",
+           "useOriginalPivot": true,
+           "shapeType": "simple-compound",
+           "grab": {
+               "grabbable": true
+           },
+           "textures": JSON.stringify(textures),
+           "lifetime": 10800
+        }, "avatar");
+        
+        var message = {
+            "channel": channel,
+            "action": "AVATAR_ENTITY_STATE",
+            "state": true
+        };
+        tablet.emitScriptEvent(JSON.stringify(message));
+    }
+
+    function clearProtest() {
+        if (protestSignID !== Uuid.NULL) {
+            Entities.deleteEntity(protestSignID);
+            protestSignID = Uuid.NULL;
+        }
+        var message = {
+            "channel": channel,
+            "action": "AVATAR_ENTITY_STATE",
+            "state": false
+        };
+        tablet.emitScriptEvent(JSON.stringify(message));
+    }
+
+    function generateSignageSign(url, shape) {
+
+        var textures = {
+            "base_color_texture": url,
+            "normalmap_texture": ROOT + "models/ROUGH_METAL_NORMAL.jpg"
+        };
+
+        var dimensions = {"x": 1, "y": 1, "z": 1};
+        switch(shape) {
+            case "S":
+                dimensions = {"x": 0.7961, "y": 3.0029, "z": 0.0500};
+                break;
+            case "H":
+                dimensions = {"x": 1.0824, "y": 3.0029, "z": 0.0500};
+                break;
+            case "V":
+                dimensions = {"x": 0.6133, "y": 3.0029, "z": 0.0500};
+                break;
+        }
+        
+        var signRotation = Quat.multiply(MyAvatar.orientation, Quat.fromVec3Degrees( { x: 0, y: 180, z: 0 } ));
+        
+        var id = Entities.addEntity({
+           "name": "Sign - ",
+           "type": "Model",
+           "position": Vec3.sum(MyAvatar.feetPosition, Vec3.multiplyQbyV(MyAvatar.orientation, { x: 0, y: 0, z: -3 })),
+           "rotation": signRotation,
+           "dimensions": dimensions,
+           "modelURL": ROOT + "models/sign_metal_" + shape.toLowerCase() + ".fst",
+           "useOriginalPivot": true,
+           "shapeType": "simple-compound",
+           "grab": {
+               "grabbable": false
+           },
+           "textures": JSON.stringify(textures)
+        }, "domain");
+        
+    }
+    
+    function generateFramedSign(url, shape, isEmissive) {
+        var textures = {
+            "base_color_texture": url
+        };
+        
+        var signFile = "framed_sign_";
+        if (isEmissive) {
+            signFile = "framed_sign_emissive_";
+            textures = {
+                "base_color_texture": url,
+                "emission_color_texture": url
+            };
+        }
+
+        var dimensions = {"x": 1, "y": 1, "z": 1};
+        switch(shape) {
+            case "S":
+                dimensions = {"x": 1.6102, "y": 1.6102, "z": 0.0322};
+                break;
+            case "H":
+                dimensions = {"x": 2.0, "y": 1.22, "z": 0.0400};
+                break;
+            case "V":
+                dimensions = {"x": 1.22, "y": 2.0, "z": 0.0400};
+                break;
+        }
+        
+        var signRotation = Quat.multiply(MyAvatar.orientation, Quat.fromVec3Degrees( { x: 0, y: 180, z: 0 } ));
+        
+        var id = Entities.addEntity({
+           "name": "Framed Sign - ",
+           "type": "Model",
+           "position": Vec3.sum(MyAvatar.position, Vec3.multiplyQbyV(MyAvatar.orientation, { x: 0, y: 0, z: -3 })),
+           "rotation": signRotation,
+           "dimensions": dimensions,
+           "modelURL": ROOT + "models/" + signFile + shape.toLowerCase() + ".fst",
+           "useOriginalPivot": true,
+           "shapeType": "box",
+           "grab": {
+               "grabbable": false
+           },
+           "textures": JSON.stringify(textures)
+        }, "domain");
+
+    }
+    
+    function sendCurrentKnownDataToUI() {
+        var message = {
+            "channel": channel,
+            "action": "INITIAL_DATA",
+            "canRez": Entities.canRez(),
+            "canRezAvatarEntities": Entities.canRezAvatarEntities(),
+            "data": Settings.getValue( channel + ".lastUsage", DEFAULT_DATA )
+        };
+        tablet.emitScriptEvent(JSON.stringify(message));
+    }
+
+    //==================================================================
+
+    function onScreenChanged(type, url) {
+        if (type === "Web" && url.indexOf(APP_URL) !== -1) {
+            appStatus = true;
+        } else {
+            appStatus = false;
+        }
+
+        if (protestSignID === Uuid.NULL) {
+            button.editProperties({
+                isActive: appStatus
+            });
+        } else {
+            button.editProperties({
+                isActive: true
+            });
+        }
+    }
+
+    function cleanup() {
+        if (protestSignID !== Uuid.NULL) {
+            Entities.deleteEntity(protestSignID);
+            protestSignID = Uuid.NULL;
+        }
+        
+        if (appStatus) {
+            tablet.gotoHomeScreen();
+            tablet.webEventReceived.disconnect(onAppWebEventReceived);
+        }
+
+        tablet.screenChanged.disconnect(onScreenChanged);
+        tablet.removeButton(button);
+    }
+
+    Script.scriptEnding.connect(cleanup);
+}());
---
+++ models/protest_sign_h.fst
@@ -0,0 +1,3 @@
+name = protest_sign_h
+filename = protest_sign_h.fbx
+materialMap = [{"mat::SIGN": {"materials":[{ "name": "SIGN", "albedo": [1,1,1], "roughness": 0.3, "metallic": 0.01, "cullFaceMode": "CULL_NONE", "defaultFallthrough": true}]}},{"mat::WOOD": {"materials":[{ "name": "WOOD", "albedo": [0.82, 0.769, 0.631], "roughness": 0.3, "metallic": 0.01, "normalMap": "woodNormal256.jpg", "cullFaceMode": "CULL_NONE"}]}}]
